3893b490aa941a0d2a7249f5c3d970703b438698
AyushKumar01 <chauhan01ayush@gmail.com>
2020-09-28 17:53:02 -0700

    error cleanup

--- Server/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const warehouseController = require("./src/controllers/warehouseController"); // import controller
-const inventoryController = require("./src/controllers/Inventory/InventoryController");
-
-router.use("/warehouses", warehouseController);
-router.use("/inventories", inventoryController);
-module.exports = router;
